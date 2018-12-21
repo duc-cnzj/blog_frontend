8d7983fb070e5b20129ca7ed68a0cbef534eaafd
duc <1025434218@qq.com>
2018-12-21 10:12:07 +0800

    socialite login

--- src/api/user.js
+++ src/api/user.js
@@ -8,6 +8,7 @@ export const login = () => ajax(`${BASE_URL}/login/github`)
 export const me = (token = '') => {
   return axios({
     method: 'post',
+    url: `${BASE_URL}/me`,
     headers: {
       Authorization: token || getToken()
     }
